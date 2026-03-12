c9e5111aabaef457bc848af089ab44169d1060a9
Rodrigo Martins <rodrigormalves@gmail.com>
2026-03-12 16:10:55 -0300

    adicionando flask para render

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot rodando"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
+
 import matplotlib.pyplot as plt
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
